6c5b835277411dd314e3011e949368288408de39
acrowder1 <31597283+acrowder1@users.noreply.github.com>
2017-09-03 14:59:04 -0500

    Practicing making changes

--- LightsOut/MainForm.cs
+++ LightsOut/MainForm.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//I added this comment from GitHub
+
 namespace LightsOut
 {
     public partial class MainForm : Form
